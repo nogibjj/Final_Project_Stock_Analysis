da4d7f3c9cb2214a0b8a96019a621fb8a104339d
udyansachdev1 <143528446+udyansachdev1@users.noreply.github.com>
2023-12-03 20:40:32 +0000

    changes

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask, jsonify, render_template, request
-import matplotlib.pyplot as plt
-import base64
-from io import BytesIO
 import pandas as pd
-import plotly.express as px
-
+import plotly.graph_objects as go
+import base64
 
 app = Flask(__name__)
 
 def generate_time_series_plot(data):
-    fig = px.line(data, x='timestamp', y='close', title='Stock Close Price Over Time', labels={'timestamp': 'Date', 'close': 'Price'})
-    fig.update_layout(showlegend=True)
+    fig = go.Figure(data=[go.Candlestick(x=data['timestamp'],
+                                         open=data['open'],
+                                         high=data['high'],
+                                         low=data['low'],
+                                         close=data['close'])])
+
+    fig.update_layout(title='Stock Candlestick Chart', xaxis_title='Date', yaxis_title='Price', showlegend=False)
 
     # Convert the plot to a div string using Plotly's to_html method
     plot_div = fig.to_html(full_html=False, include_plotlyjs='cdn')
@@ -28,13 +30,13 @@ def index():
 def predict():
     try:
         ticker = request.form['ticker']
-        # retreive historical data
+        # Retrieve historical data
         stock_data = pd.read_csv('Final_Merge.csv')
         stock_data = stock_data[stock_data['Symbol'] == ticker]
 
-        # Calculate the 10-day moving average
+        # Calculate the 10-day rolling average
         stock_data['10_day_MA'] = stock_data['close'].rolling(window=10).mean()
-
+        
         # Get the last row of the data (latest date)
         last_row = stock_data.iloc[0]
 
@@ -43,22 +45,24 @@ def predict():
             'Symbol': ticker,
             'RecentDate': last_row['timestamp'],
             'Last Close Price': last_row['close'],
-            '10 Day moving Average': last_row['10_day_MA'],
             'Open': last_row['open'],
             'High': last_row['high'],
             'Low': last_row['low'],
             'Country': last_row['Country'],
             'Sector': last_row['Sector'],
-            'Industry': last_row['Industry']
+            'Industry': last_row['Industry'],
+            '10_day_MA': last_row['10_day_MA']  # Adding 10-day MA to prediction
         }
 
         # Generate time series plot
         plot_url = generate_time_series_plot(stock_data)
 
-        return render_template('stock_prediction.html', prediction=prediction, ticker=ticker,plot_url=plot_url)
-
+        return render_template('stock_prediction.html', prediction=prediction, ticker=ticker, plot_url=plot_url)
+    
     except Exception as e:
+        print(e)  # Print the actual exception for debugging
         return jsonify({'error': str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
