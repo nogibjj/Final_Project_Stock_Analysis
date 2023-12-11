cf1ff29a492e683fd0d25392a9ec40d697517217
Revanth Chowdary <46459100+revanth7667@users.noreply.github.com>
2023-12-11 09:37:32 +0000

    updated test

--- test_main.py
+++ test_main.py
@@ -1,36 +1,17 @@
 # To Test if the Sql connection is working and the data is being pulled
 
-from databricks import sql
 import pandas as pd
+import finnhub
 
 
 def test_connection():
-    connection = sql.connect(
-        server_hostname="adb-1767346969741129.9.azuredatabricks.net",
-        http_path="/sql/1.0/warehouses/a20e8d116459b6d7",
-        access_token="dapib2db61506df9e84e6e932b33e8600dd4-3",
-    )
+    finnhub_client = finnhub.Client(
+            api_key="clla0nhr01qhqdq2t4e0clla0nhr01qhqdq2t4eg"
+        )
 
-    ticker = "AAPL"  # make this dynamic in the code
-    rows = 1000  # decide how many rows you want to pull
-
-    cursor = connection.cursor()
-    query = (
-        "SELECT * FROM historical_data where Ticker = '"
-        + ticker
-        + "' order by timestamp desc limit "
-        + str(rows)
-    )
-    cursor.execute(query)
-    df = pd.DataFrame(cursor.fetchall())
-    df.columns = [desc[0] for desc in cursor.description]
-
-    cursor.close()
-    connection.close()
-
-    print(df.head())
-
-    assert df.shape[0] == rows
+    res = finnhub_client.quote('AAPL')
+    df = pd.DataFrame(res, index=[0])
+    assert df.shape[0] > 0
 
 
 if __name__ == "__main__":
